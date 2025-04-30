bd5184282bdde461dc9eb73a02d1206e205f70c6
Ravwvil <rawil.kazeev@gmail.com>
2025-04-30 16:32:38 +0300

    fix: improve serverID handling

--- analyzer/internal/parser/parser.go
+++ analyzer/internal/parser/parser.go
@@ -5,48 +5,76 @@ import (
 	"github.com/Linch-JG/Distributed-Log-Analysis-Framework/analyzer/internal/models"
 	"regexp"
 	"strconv"
+	"strings"
 	"time"
 )
 
-var logPattern = regexp.MustCompile(`^([^:]+): ([\d\.]+) - \S+ \[([^\]]+)\] "([A-Z]+) ([^"]+) HTTP/\d\.\d" (\d{3}) \d+ "[^"]*" "([^"]+)"$`)
+var serverLogPattern = regexp.MustCompile(`^([^:]+): ([\d\.]+) - \S+ \[([^\]]+)\] "([A-Z]+) ([^"]+) HTTP/\d\.\d" (\d{3}) \d+ "[^"]*" "([^"]+)"$`)
+var noServerLogPattern = regexp.MustCompile(`^([\d\.]+) - \S+ \[([^\]]+)\] "([A-Z]+) ([^"]+) HTTP/\d\.\d" (\d{3}) \d+ "[^"]*" "([^"]+)"$`)
 
 // Consider this nginx log format(in one line):
-// serverid 192.168.1.1 - - [22/Apr/2025:13:37:42 +0000]
-// "GET /home HTTP/1.1" 200 532 "-" "Mozilla/5.0"
+// serverID: 192.168.1.1 - - [22/Apr/2025:13:37:42 +0000] "GET /home HTTP/1.1" 200 532 "-" "Mozilla/5.0"
 
 func ParseRawLog(line string) (*models.Log, error) {
-	matches := logPattern.FindStringSubmatch(line)
+	if matches := serverLogPattern.FindStringSubmatch(line); matches != nil && len(matches) == 8 {
+		serverID := matches[1]
+		ip := matches[2]
+		timeStr := matches[3]
+		method := matches[4]
+		endpoint := matches[5]
+		statusStr := matches[6]
+		userAgent := matches[7]
 
-	const expectedGroups = 8
-	if matches == nil || len(matches) != expectedGroups {
-		return nil, fmt.Errorf("failed to parse log line: %s", line)
+		timestamp, err := time.Parse("02/Jan/2006:15:04:05 -0700", timeStr)
+		if err != nil {
+			return nil, fmt.Errorf("failed to parse time: %w", err)
+		}
+
+		status, err := strconv.Atoi(statusStr)
+		if err != nil {
+			return nil, fmt.Errorf("failed to parse status code: %w", err)
+		}
+
+		return &models.Log{
+			ServerID:  serverID,
+			IP:        ip,
+			Timestamp: timestamp,
+			Method:    method,
+			Endpoint:  endpoint,
+			Status:    status,
+			UserAgent: userAgent,
+		}, nil
 	}
 
-	serverID := matches[1]
-	ip := matches[2]
-	timeStr := matches[3]
-	method := matches[4]
-	endpoint := matches[5]
-	statusStr := matches[6]
-	userAgent := matches[7]
+	if matches := noServerLogPattern.FindStringSubmatch(line); matches != nil && len(matches) == 7 {
+		serverID := "server_default"
+		ip := matches[1]
+		timeStr := matches[2]
+		method := matches[3]
+		endpoint := matches[4]
+		statusStr := matches[5]
+		userAgent := matches[6]
 
-	timestamp, err := time.Parse("02/Jan/2006:15:04:05 -0700", timeStr)
-	if err != nil {
-		return nil, fmt.Errorf("failed to parse time: %w", err)
+		timestamp, err := time.Parse("02/Jan/2006:15:04:05 -0700", timeStr)
+		if err != nil {
+			return nil, fmt.Errorf("failed to parse time: %w", err)
+		}
+
+		status, err := strconv.Atoi(statusStr)
+		if err != nil {
+			return nil, fmt.Errorf("failed to parse status code: %w", err)
+		}
+
+		return &models.Log{
+			ServerID:  serverID,
+			IP:        ip,
+			Timestamp: timestamp,
+			Method:    method,
+			Endpoint:  endpoint,
+			Status:    status,
+			UserAgent: userAgent,
+		}, nil
 	}
 
-	status, err := strconv.Atoi(statusStr)
-	if err != nil {
-		return nil, fmt.Errorf("failed to parse status code: %w", err)
-	}
-
-	return &models.Log{
-		ServerID:  serverID,
-		IP:        ip,
-		Timestamp: timestamp,
-		Method:    method,
-		Endpoint:  endpoint,
-		Status:    status,
-		UserAgent: userAgent,
-	}, nil
+	return nil, fmt.Errorf("failed to parse log line: %s", line)
 }
